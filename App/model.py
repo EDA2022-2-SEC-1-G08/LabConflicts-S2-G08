63fdeca03473f294d39de714abe375f579295ef7
Juan Pablo Baldion <j.baldion@uniandes.edu.co>
2022-08-19 07:25:43 -0500

    Cambios del Estudiante 3

--- App/model.py
+++ App/model.py
@@ -117,24 +117,24 @@ def addBookTag(catalog, booktag):
 
 def bookSize(catalog):
     # TODO Modificaciones de Est-3 en el Lab 2
-    pass
+    return lt.size(catalog["books"])
 
 
 def tagSize(catalog):
     # TODO Modificaciones de Est-3 en el Lab 2
-    pass
+    return lt.size(catalog["tags"])
 
 
 def bookTagSize(catalog):
     # TODO Modificaciones de Est-3 en el Lab 2
-    pass
+    return lt.size(catalog["book_tags"])
 
 
 def firstBook(catalog):
     # TODO Modificaciones de Est-3 en el Lab 2
-    pass
+    return lt.firstElement(catalog["books"])
 
 
 def lastBook(catalog):
     # TODO Modificaciones de Est-3 en el Lab 2
-    pass
+    return lt.lastElement(catalog["books"])
